d44f3ffd0d6de5d4abea440d8ec212452342dfa2
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2022-12-30 17:13:49 +0700

    add class node, define variabel needed

--- BinarySearchTree_090/Program.cs
+++ BinarySearchTree_090/Program.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree_090
 {
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //constructor for the node class
+        
+    }
     internal class Program
     {
         static void Main(string[] args)
